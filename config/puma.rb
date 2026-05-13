b6e9f950d13d23be138bd54adb7fbc03f2e6ddbb
lineoffligbot <4628864+lineoffligbot@users.noreply.github.com>
2026-05-13 18:38:58 +0200

    Make Puma server PORT-aware
    Allows Docker image to respect dynamically assigned PORT environment
    variable from PaaS platforms (Heroku, Fly.io, etc.). Default 8080 is
    set at Dockerfile ENV layer to maintain compatibility with existing
    single-VM deployments.

--- config/puma.rb
+++ config/puma.rb
@@ -4,7 +4,7 @@ workers Integer(ENV.fetch("WORKER_PROCESSES", 4))
 threads_count = Integer(ENV.fetch("MAX_THREADS", 5))
 threads threads_count, threads_count
 
-port 8080
+port Integer(ENV.fetch("PORT", 8080))
 worker_timeout 10
 preload_app!
 
